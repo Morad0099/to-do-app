111bae56a174e9f2612907f4c15602ad97240f47
Musah Morad <musahmorad01@gmail.com>
2024-08-02 17:02:53 -0400

    "Added Mongoose connection and GET route for root URL, updated Express setup and view engine configuration"

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
 //init app
 const app = express();
 
+const connectionUrl = "mongodb://localhost:27017/todo";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
+
 //view engine
 app.set("view engine", "ejs");
 
+app.get("/", (req, res, next) => {
+    try {
+        res.render("index")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 //listen server
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`); 
-})
+  console.log(`Server is running on ${PORT}`);
+});
